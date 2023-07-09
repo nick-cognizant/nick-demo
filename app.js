98acb975069fe1c223581b543ddecd0da887ae62
Hao Fu <haofu@Haos-Air-2.attlocal.net>
2023-07-09 17:20:56 -0400

    font and bc

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
+// const express = require('express');
+// const app = express();
+// const port = 8080;
+
+// app.get('/', (req, res) => res.send('Hello, World!'));
+
+// app.listen(port, () => {
+//   console.log(`App running at http://localhost:${port}`);
+// });
+
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  const font = process.env.FONT || 'Arial, sans-serif';
+  const backgroundColor = process.env.BACKGROUND_COLOR || '#f0f0f0';
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {
+                background-color: ${backgroundColor}; /* this is the background color */
+                font-family: ${font}; /* this is the font */
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+    </html>
+  `);
+});
+
 const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello, World!'));
-
 app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`)
 });
